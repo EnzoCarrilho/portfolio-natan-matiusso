501ef20e1ef9e4ff08b4822e9dd08d11436ccfdf
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-08-12 21:06:18 -0300

    feat: Estilizando textos da Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
 
       <Navbar></Navbar>
 
-      <main className="h-full w-full py-4 gap-20">
+      <main className="h-full w-full py-4">
 
         <div className="flex px-32">
 
@@ -22,8 +22,8 @@ function Home() {
           </div>
 
           <div className="flex flex-col items-center justify-around w-1/2">
-            <h2>Natan Matiusso</h2>
-            <p>
+            <h2 className="text-2xl text-primary font-bold">Natan Matiusso</h2>
+            <p className="text-center text-secondary-text">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
               text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took 
               a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, 
@@ -37,8 +37,8 @@ function Home() {
          <div className="flex px-32">
 
           <div className="flex flex-col items-center justify-around w-1/2">
-            <h2>Natan Matiusso</h2>
-            <p>
+            <h2 className="text-2xl text-primary font-bold">Natan Matiusso</h2>
+            <p className="text-center text-secondary-text">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
               text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took 
               a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, 
